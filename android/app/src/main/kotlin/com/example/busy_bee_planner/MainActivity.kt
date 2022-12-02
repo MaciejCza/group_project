5cd114a98f4bac38a675d1808e9dd6be4ab9dcae
MaciejCza <maciej.czarnuch@wp.pl>
2022-12-02 08:56:45 +0100

    praca praca

---
+++ android/app/src/main/kotlin/com/example/busy_bee_planner/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.busy_bee_planner
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/app.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'main.dart';
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'BusyBeePlaner',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const (title: 'HomePage'),
+    );
+  }
+}
