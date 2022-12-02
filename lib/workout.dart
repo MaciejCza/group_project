43cee43e5167126ed6981f6ee258f830dc734117
Rndom8 <cecdermer@gmail.com>
2022-12-02 11:22:01 +0100

    workout

--- lib/workout.dart
+++ lib/workout.dart
@@ -5,10 +5,37 @@ import 'package:flutter/material.dart';
 class Workout extends StatelessWidget {
   const Workout({Key? key}) : super(key: key);
 
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: const Text('AppBar Demo')),
-        body: Container(child: Container(child: Text('Hello World'))));
+        body: Container(
+            height: MediaQuery.of(context).size.height,
+            child: Container(
+                height: MediaQuery.of(context).size.height,
+                child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+                      Container(
+                        height: MediaQuery.of(context).size.height * 0.15,
+                        decoration: BoxDecoration(color: Colors.yellow),
+                      ),
+                      SizedBox(
+                        height: MediaQuery.of(context).size.height * 0.5,
+                        child: const Text(
+                          'Hello World',
+                          style: TextStyle(fontSize: 20),
+                        ),
+                      ),
+                      SizedBox(
+                          height: MediaQuery.of(context).size.height * 0.5,
+                          child: const ClipRRect(
+                            borderRadius: BorderRadius.all(Radius.circular(50)),
+                            child: LinearProgressIndicator(
+                              value: 0.7,
+                              valueColor: AlwaysStoppedAnimation<Color>(
+                                  Color(0xff00ff00)),
+                              backgroundColor: Color(0xffD6D6D6),
+                            ),
+                          ))
+                    ]))));
   }
 }
