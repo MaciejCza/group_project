572a10ac319dbb68d9056a229f91b41e9eab04dc
Rndom8 <cecdermer@gmail.com>
2022-12-02 09:20:22 +0100

    workout

---
+++ lib/workout.dart
@@ -0,0 +1,25 @@
+import 'main.dart';
+import 'app.dart';
+import 'package:flutter/material.dart';
+
+class Calc extends StatelesWidget {
+
+}
+
+class _CalcState extends State<Calc> {
+
+
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SizedBox(
+          height: MediaQuery.of(context).size.height,
+          child: Column(
+
+          )),
+    );
+  }
+}
+
+
+
