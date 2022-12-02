1ccafcc4cbad67bf2c145dd036477e9ce7b87455
MaciejCza <maciej.czarnuch@wp.pl>
2022-12-02 09:57:55 +0100

    praca praca

--- lib/calendar.dart
+++ lib/calendar.dart
@@ -13,7 +13,9 @@ class Calendar extends StatelessWidget {
           Container(child:
             SingleChildScrollView(child:
               Column(children: [
-                Card(margin: const EdgeInsets.all(8.0),elevation: 5.0,shape: const RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(10)),side: BorderSide(color: Colors.black,width: 2.0)))
+                Card(margin: const EdgeInsets.all(8.0),elevation: 5.0,shape: const RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(10)),side: BorderSide(color: Colors.black,width: 2.0),),
+                  child: TableCalendar()
+                ),
               ],
 
               )
