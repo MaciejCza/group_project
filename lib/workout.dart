514e38f9714983b9003957a80d823cb306ae919d
Rndom8 <cecdermer@gmail.com>
2022-12-14 11:24:24 +0100

    workout

--- lib/workout.dart
+++ lib/workout.dart
@@ -22,6 +22,9 @@ int adding = 0;
 var date1 = const Text('');
 double size1 = 0;
 var title1 = const Text('');
+var title2 = const Text('');
+var title3 = const Text('');
+var title4 = const Text('');
 var check1 = const Text('');
 bool col = false;
 
@@ -61,10 +64,16 @@ class _WorkoutState extends State<Workout> {
             checkSize = 1;
           } else if (adding == 2) {
             checkSize1 = 1;
+            title2 =
+                Text('  ${controler.text}', style: TextStyle(fontSize: 20));
           } else if (adding == 3) {
             checkSize2 = 1;
+            title3 =
+                Text('  ${controler.text}', style: TextStyle(fontSize: 20));
           } else if (adding == 4) {
             checkSize3 = 1;
+            title4 =
+                Text('  ${controler.text}', style: TextStyle(fontSize: 20));
           }
         });
       },
@@ -251,8 +260,7 @@ class _WorkoutState extends State<Workout> {
                                 semanticLabel:
                                     'Text to announce in accessibility modes',
                               ),
-                              Text('  ${controler.text}',
-                                  style: TextStyle(fontSize: 20)),
+                              title2,
                               const Expanded(child: SizedBox()),
                               Theme(
                                   data: ThemeData(
@@ -308,8 +316,7 @@ class _WorkoutState extends State<Workout> {
                                     semanticLabel:
                                         'Text to announce in accessibility modes',
                                   ),
-                                  Text('  ${controler.text}',
-                                      style: TextStyle(fontSize: 20)),
+                                  title3,
                                   const Expanded(child: SizedBox()),
                                   Theme(
                                       data: ThemeData(
@@ -361,8 +368,7 @@ class _WorkoutState extends State<Workout> {
                                     semanticLabel:
                                         'Text to announce in accessibility modes',
                                   ),
-                                  Text('  ${controler.text}',
-                                      style: TextStyle(fontSize: 20)),
+                                  title4,
                                   const Expanded(child: SizedBox()),
                                   Theme(
                                       data: ThemeData(
